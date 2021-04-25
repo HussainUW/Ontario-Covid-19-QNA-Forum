75e9f13a68caccf84480135e8cf9f10d455063d1
Hussain Kuvawala <56942119+HussainUW@users.noreply.github.com>
2021-04-25 17:35:11 -0400

    Adding routes for the posts blueprint

---
+++ posts/routes.py
@@ -0,0 +1,73 @@
+from flask import (render_template, url_for, flash,
+				   redirect, request, abort, Blueprint)
+from flask_login import current_user, login_required
+from Flask_Blog import db
+from Flask_Blog.models import Post, Comment
+from Flask_Blog.posts.forms import PostForm, AddCommentForm
+
+posts = Blueprint('posts', __name__)
+
+@posts.route("/post/new", methods=['GET', 'POST'])
+@login_required
+def new_post():
+	form = PostForm()
+	if form.validate_on_submit():
+		post = Post(title = form.title.data, content = form.content.data, author = current_user)
+		db.session.add(post)
+		db.session.commit()
+		flash('Your post has been created!','success')
+		return redirect(url_for('main.home'))
+	return render_template('create_post.html', title = 'New Post', form = form, legend='New Post')
+
+@posts.route("/post/<int:post_id>", methods=['GET', 'POST'])
+def post(post_id):
+	post = Post.query.get_or_404(post_id)
+	comments = Comment.query.all()
+	return render_template('post.html', title=post.title, post=post, comments=comments)
+
+@posts.route("/post/<int:post_id>/update", methods=['GET', 'POST'])
+@login_required
+def update_post(post_id):
+	post = Post.query.get_or_404(post_id)
+	if post.author != current_user:
+		abort(403)
+	form = PostForm()
+	if form.validate_on_submit():
+		post.title = form.title.data
+		post.content = form.content.data
+		db.session.commit()
+		flash('Your post has been updated!', 'success')
+		return redirect(url_for('posts.post', post_id=post.id))
+	elif request.method == 'GET':
+		form.title.data = post.title
+		form.content.data = post.content
+	return render_template('create_post.html', title = 'Update Post', form = form, 
+		legend='Update Post')
+
+@posts.route("/post/<int:post_id>/delete", methods=['POST'])
+@login_required
+def delete_post(post_id):
+	post = Post.query.get_or_404(post_id)
+	if post.author != current_user:
+		abort(403)
+	db.session.delete(post)
+	db.session.commit()
+	flash('Your post has been successfully deleted!', 'success')
+	return redirect(url_for('main.home'))
+
+@posts.route("/post/<int:post_id>/comment", methods=["GET", "POST"])
+@login_required
+def comment_post(post_id):
+	ID = post_id
+	form = AddCommentForm()
+	if request.method == 'POST':
+		if form.validate_on_submit():
+			comment = Comment(body=form.body.data, responder=form.responder.data, post_id=ID)
+			db.session.add(comment)
+			db.session.commit()
+			flash("Your comment has been added to the post", "success")
+			return redirect(url_for("posts.post", post_id=ID))
+	if request.method == 'GET':
+		form.responder.data = "Anonymous"
+	return render_template("comment_post.html", title="Comment Post", form=form, post_id=post_id)
+
